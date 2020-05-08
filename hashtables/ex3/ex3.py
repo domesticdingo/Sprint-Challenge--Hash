12771f0fce24d6cbc9555fb54a6edddfa8a3d189
Indigo Richards <thedomesticdingo@gmail.com>
2020-05-08 13:29:36 -0700

    MVP
    Completed ex1, ex2, and ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,12 +1,19 @@
-import math
-
-cache = {}
-
-
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
+    hash_table = {}
+    result = []
+
+    # Go through each array, count 1 each time the number is shown
+    for array in arrays:
+        for val in array:
+            if val not in hash_table:
+                hash_table[val] = 1
+            else:
+                hash_table[val] += 1
+
+    # If the count for a number is equal to number of arrays, add to returned result
+    for key, value in hash_table.items():
+        if value == len(arrays):
+            result.append(key)
 
     return result
 
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,9 +1,5 @@
 def finder(files, queries):
 
-    """
-    YOUR CODE HERE
-    """
-
     return result
 
 
